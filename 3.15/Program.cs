3b51dbaa7f2661e13761f0891e6df7a3a716ab76
Yasuohasagi7rank <112807477+Yasuohasagi7rank@users.noreply.github.com>
2022-11-21 00:56:56 +0300

    Add files via upload

---
+++ 3.15/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _3._15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool A, B, C;
+            bool Aitog, Bitog, Citog;
+            A = true;
+            B = false;
+            C = false;
+
+            Aitog = !A || !B;
+            Bitog = A & (A || !B);
+            Citog = (!A || B) & B;
+
+            Console.WriteLine("Результат: \n{0} \n{1} \n{2} ", Aitog, Bitog, Citog);
+            Console.ReadLine();
+        }
+    }
+}
